2f2b94fdb2c0e6054641c87457fa0f8666bf9b99
Atanu Hazra <imatanuhazra@gmail.com>
2025-03-24 20:36:50 +0530

    Add order and product info endpoints with serializers for detailed responses

--- restify/api/serializers.py
+++ restify/api/serializers.py
@@ -7,11 +7,9 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'name',
-            'description',
             'price',
             'stock'
         )
-        # fields = "__all__"
 
     def validate_price(self, value):
         if value <= 0:
@@ -19,3 +17,54 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than 0."
             )
         return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):\
+    # Nested serializer to include full product details (use only when needed)
+    product = ProductSerializer()
+
+    # Instead of returning the whole product object, we can extract only necessary fields
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        source='product.price'
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product',        # Returns full product details (nested)
+            'quantity',
+            'product_name',   # Sends only product name in response
+            'product_price',  # Sends only product price in response
+            'item_subtotal'   # Uses model's property to calculate subtotal
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'items',
+            'total_price'
+        )
+
+
+# using generic serializer
+class ProductInfoSerializer(serializers.Serializer):
+    # get all products, count of products, max price
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
