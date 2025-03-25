5c6373d76397ccc42bfbab12d4344bd4d98f66d4
Atanu Hazra <imatanuhazra@gmail.com>
2025-03-25 14:37:31 +0530

    Add generic views for products and orders; register Order model with inline OrderItems in admin

--- restify/api/urls.py
+++ restify/api/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
+    path('products/all/', views.ProductListAPIView.as_view()), # Using Generic View
     path('products/info/', views.product_info),
     path('products/<int:pk>/', views.product_detail),
+    path('products/all/<int:pk>/', views.ProductDetailAPIView.as_view()), # Using Generic View
     path('orders/', views.order_list),
+    path('orders/all/', views.OrderListAPIView.as_view()), # Using Generic View
+    path('user-orders/', views.UserOrderListAPIView.as_view()), # Using Generic View
 ]
 
--- restify/api/views.py
+++ restify/api/views.py
@@ -5,6 +5,7 @@ from .models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 
 @api_view(['GET'])
@@ -13,6 +14,11 @@ def product_list(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+# Generic view for the same task
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
 
 @api_view(['GET'])
 def product_detail(request, pk):
@@ -20,12 +26,33 @@ def product_detail(request, pk):
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
+# Generic view for the same task
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
 
 @api_view(['GET'])
 def order_list(request):
     orders = Order.objects.prefetch_related('items__product')
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
+
+# generic view
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+
+
+# getting orders of the current user
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        return qs.filter(user=user)
 
 
 @api_view(['GET'])
